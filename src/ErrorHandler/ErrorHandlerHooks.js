f8c8134079179450c121285565934ffcf35443f3
anaselalami <anaselalamikh@gmail.com>
2019-06-04 15:23:00 +0100

    modify email regex

--- src/ErrorHandler/ErrorHandlerHooks.js
+++ src/ErrorHandler/ErrorHandlerHooks.js
@@ -41,7 +41,7 @@ let errorHanlderHooks = {
   },
 
   isEmail(val) {
-    let regx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    let regx = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
     return regx.test(val);
   },
 
